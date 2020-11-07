feefc0cb384ab422062dc5d93a154a68eddd53ad
Alexius Lim <alexiuslim@Alexiuss-MacBook-Pro.local>
2020-11-07 16:54:58 +0800

    Added events migration

---
+++ database/migrations/2020_11_01_145947_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('category');
+            $table->text('thumbnail');
+            $table->date('fromDate');
+            $table->date('toDate');
+            $table->text('content');
+            $table->string('status', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
